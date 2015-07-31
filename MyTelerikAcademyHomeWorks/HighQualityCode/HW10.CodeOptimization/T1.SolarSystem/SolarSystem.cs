645c82664e49a00ec995ace583fbc1a461a3ce42
studware <angela.teneva@gmail.com>
2015-07-31 23:07:06 +0300

    HW10.CodeOptimization

---
+++ MyTelerikAcademyHomeWorks/HighQualityCode/HW10.CodeOptimization/T1.SolarSystem/SolarSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1.SolarSystem
+{
+    class SolarSystem
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please, see the solar system optimization snapshots in the .png files:\n");
+            Console.WriteLine("T1.SolarSystemBeforeFix.png");
+            Console.WriteLine("T1.SolarSystemJustTrace.png");
+            Console.WriteLine("T1.SolarSystemAfterFix.png\n");
+        }
+    }
+}
