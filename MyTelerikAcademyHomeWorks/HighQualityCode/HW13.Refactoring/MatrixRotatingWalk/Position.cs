73e36b6be6b4eb14e7987d5b77b57821cc86f2bf
studware <angela.teneva@gmail.com>
2015-08-06 23:54:42 +0300

    HW13.Refactoring

---
+++ MyTelerikAcademyHomeWorks/HighQualityCode/HW13.Refactoring/MatrixRotatingWalk/Position.cs
@@ -0,0 +1,37 @@
+﻿namespace MatrixRotatingWalk
+{
+    public class Position
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Position"/> class.
+        /// </summary>
+        /// <param name="row">The row coordinate.</param>
+        /// <param name="col">The col coordinate.</param>
+        public Position(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        /// <summary>
+        /// Gets or sets the row coordinate of the position.
+        /// </summary>
+        public int Row { get; set; }
+
+        /// <summary>
+        /// Gets or sets the col coordinate of the position.
+        /// </summary>
+        public int Col { get; set; }
+
+        /// <summary>
+        /// Updates the coordinates of the position by adding
+        /// the specified delta.
+        /// </summary>
+        /// <param name="delta">The delta used for the update.</param>
+        public void Update(Delta delta)
+        {
+            this.Row += delta.Row;
+            this.Col += delta.Col;
+        }
+    }
+}
