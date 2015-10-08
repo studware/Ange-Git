9fc853547362fd5afd13dee8aed20e6ee7d21ae0
studware <angela.teneva@gmail.com>
2015-10-08 17:51:59 +0300

    HW3.Processing-JSON-in-.NET

---
+++ MyTelerikAcademyHomeWorks/DataBase/HW3.Processing-JSON-in-.NET/Processing-JSON-in-.NET/IVideo.cs
@@ -0,0 +1,12 @@
+﻿namespace Processing_JSON_in_.NET
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    
+    public interface IVideo
+    {
+        string Title { get; set; }
+    }
+}
