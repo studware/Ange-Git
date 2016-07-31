b4bf84c37692c99ea84c1b5b84e5067165d1eba6
studware <angela.teneva@gmail.com>
2016-08-01 02:07:03 +0300

    Santase sourse code downloaded for implementation of unit tests (HW1.Unit-Testing)

---
+++ MyTelerikAcademyHomeWorks/Unit-Testing/HW1.Unit-Testing/Santase/Santase.Logic/Cards/IDeck.cs
@@ -0,0 +1,17 @@
+﻿namespace Santase.Logic.Cards
+{
+    public interface IDeck
+    {
+        Card GetNextCard();
+
+        Card GetTrumpCard { get; }
+
+        void ChangeTrumpCard(Card newCard);
+
+        int CardsLeft { get; }
+    }
+}
+
+
+
+
