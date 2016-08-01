ccd4d96f28005eb089dbca096baaf735f6052d72
studware <angela.teneva@gmail.com>
2016-08-01 19:48:37 +0300

    HW1.Unit-Testing/Santase/Santase.Tests/DeckTest.cs

---
+++ MyTelerikAcademyHomeWorks/Unit-Testing/HW1.Unit-Testing/Santase/Santase.Tests/DeckTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Santase.Logic;
+using Santase.Logic.Cards;
+
+
+namespace Santase.Tests
+{
+    [TestFixture]
+    public class DeckTest
+    {
+        private const int DeckSize=24;
+
+        [TestCase(DeckSize)]
+        public void DrawNextCardFromEmptyDeckShouldThrowException(int cardsToBeDrawn)
+        {
+            var deck = new Deck();
+            for (int i = 0; i < cardsToBeDrawn; i++)
+            {
+                deck.GetNextCard();                    
+            }
+            Assert.Throws<InternalGameException>(() => deck.GetNextCard());
+        }
+
+        [Test]
+        public void FullDeckShouldContain_DeckSize_Cards()
+        {
+            var deck = new Deck();
+            Assert.AreEqual(DeckSize, deck.CardsLeft, "The full deck should contain " + DeckSize + " cards.");
+        }
+
+        [Test]
+        public void TestCountOfCardsInNewDeckInstanceIsGreaterThanZero()
+        {
+            Deck gameDeck = new Deck();
+            int initialCardsCount = gameDeck.CardsLeft;
+            Assert.Greater(initialCardsCount, 0);
+        }
+        [Test]
+        public void TestNextCardGotFromTheDeckIsNotNull()
+        {
+            Deck gameDeck = new Deck();
+            int initialCardsCount = gameDeck.CardsLeft;
+            Assert.Greater(initialCardsCount, 0);
+            Card nextCard = gameDeck.GetNextCard();
+            Assert.IsNotNull(nextCard);
+        }
+
+        [Test]
+        public void TestValidCardsCountAfterGettingTheNextCardFromTheDeck()
+        {
+            Deck gameDeck = new Deck();
+            int initialCardsCount = gameDeck.CardsLeft;
+            Card nextCard = gameDeck.GetNextCard();
+            Assert.AreEqual(initialCardsCount - 1, gameDeck.CardsLeft);
+        }
+
+        [Test]
+        public void TestTrumpCardIsAtTheTopOfDeck()
+        {
+            Deck deck = new Deck();
+            int cardsCount = deck.CardsLeft;
+            Card topCard = deck.GetNextCard();
+            for (int i = 1; i < cardsCount; i++)
+            {
+                topCard = deck.GetNextCard();
+            }
+            Assert.AreEqual(topCard, deck.GetTrumpCard);
+            Assert.AreSame(topCard, deck.GetTrumpCard);
+        }
+
+        [Test]
+        [TestCase(CardSuit.Spade, CardType.King)]
+        [TestCase(CardSuit.Heart, CardType.Ace)]
+        [TestCase(CardSuit.Diamond, CardType.Jack)]
+        [TestCase(CardSuit.Heart, CardType.Queen)]
+        [TestCase(CardSuit.Diamond, CardType.Ten)]
+        public void TestChangeTrumpCardShouldProperlyUpdateBothTrumpCardAndDeckTopCard(CardSuit suit, CardType type)
+        {
+            Deck deck = new Deck();
+            Card newTrumpCard = new Card(suit, type);
+            deck.ChangeTrumpCard(newTrumpCard);
+            int cardsCount = deck.CardsLeft;
+            Card topCard = deck.GetNextCard();
+            for (int i = 1; i < cardsCount; i++)
+            {
+                topCard = deck.GetNextCard();
+            }
+            Assert.AreEqual(topCard, newTrumpCard);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(10)]
+        [TestCase(24)]
+        public void TestGettingCardsFromDeckShouldProperlyUpdateTheCountOfCardsLeft(int countOfCardsToGet)
+        {
+            Deck deck = new Deck();
+            Card card = deck.GetNextCard();
+            int cardsLeftCount = deck.CardsLeft;
+            for (int i = 1; i < countOfCardsToGet; i++)
+            {
+                card = deck.GetNextCard();
+                cardsLeftCount--;
+            }
+            Assert.AreEqual(deck.CardsLeft, cardsLeftCount);
+        }
+
+    }
+}
